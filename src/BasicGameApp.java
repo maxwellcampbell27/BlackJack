811652e16c24cbceb822fd9e4fcaa0a5409650f5
maxwellcampbell27 <maxwell_campbell27@milton.edu>
2026-05-08 14:55:06 -0400

    Added Methods

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -4,4 +4,38 @@ public Card[] deck;
 public Player p1;
 public Dealer d1;
 public Boolean gameOn;
+
+
+
+    public static void main(String[] args) {
+
+
+        new BasicGameApp();
+    }
+
+
+    public BasicGameApp(){
+        System.out.println("Welcome to Black Jack");
+        deck = new Card[52];
+
+        //todo: fill the deck arrauy with your favortie card
+
+        for(int i=0; i < deck.length; i++ ){
+            deck[i] = new Card(10, i, "Hearts");
+            deck[i].printInfo();
+
+        }
+
+}
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/Card.java
+++ src/Card.java
@@ -3,4 +3,36 @@ public class Card {
     public String name;
     public String suit;
 
+    //todo: make a constructor for the card that has three parama
+    //feed the parameters into the instance variables
+
+    public Card(int pValue, int pName, String pSuit){
+        value = pValue;
+        if(pName == 0){
+            name = " Ace ";
+            value = 11;
+        } else if (pName == 1){
+            name = "two";
+            value = 2;
+        }
+        name = int Name;
+        suit = pSuit;
+
+
+
+    }
+
+
+    //todo: make a printInfo method that prints the card info
+    // the ___ of ___ is worth __ points
+    public void printInfo(){
+
+        System.out.println(" The " + name + " of " + suit + " is worth " + value + " points ");
+
+
+    }
+
+
+
+
 }
